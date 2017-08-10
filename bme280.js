6350effac8ecf358c9602a3956d9ed85369262cc
ihysk <sirouma.09@gmail.com>
2017-08-10 23:56:07 +0900

    Fixed eslint errors

--- bme280.js
+++ bme280.js
@@ -1,42 +1,34 @@
-'use strict';
+const BME280 = require('node-bme280');
+const M2X = require('m2x');
 
-var BME280 = require('node-bme280');
-var M2X = require('m2x')
+const barometer = new BME280({ address: 0x76 });
 
-var barometer = new BME280({address: 0x76});
-
-var key = process.env.M2X_KEY;
-var deviceId = process.env.M2X_DEVICEID;
-var tempStreamId = 'hysk-room-temp';
-var humidStreamId = 'hysk-room-humidity';
-var m2x = new M2X(key);
+const key = process.env.M2X_KEY;
+const deviceId = process.env.M2X_DEVICEID;
+const tempStreamId = 'hysk-room-temp';
+const humidStreamId = 'hysk-room-humidity';
+const m2x = new M2X(key);
 
 barometer.begin(function(err) {
-    if (err) {
-	console.info('error initializing barometer', err);
-	return;
-    }
+  if (err) {
+    console.info('error initializing barometer', err);
+    return;
+  }
 
-    console.info('barometer running');
+  console.info('barometer running');
 
-    barometer.readPressureAndTemparature(function(err, pressure, temperature, humidity) {
+  barometer.readPressureAndTemparature(function(err, pressure, temperature, humidity) {
+    console.info(
+      'temp:', temperature.toFixed(2),
+      '℃  pressur', (pressure / 100).toFixed(2),
+      'hPa  hum:', humidity.toFixed(2), '%');
 
-	console.info(
-	    'temp:',
-	    temperature.toFixed(2),
-	    '℃  pressure:',
-	    (pressure / 100).toFixed(2),
-	    'hPa  hum:',
-	    humidity.toFixed(2),
-	    '%'
-	);
-
-	m2x.devices.setStreamValue(deviceId, tempStreamId, {"value":temperature.toFixed(2)}, function(response) {
-    	    console.log(response.json);
-	});
-
-	m2x.devices.setStreamValue(deviceId, humidStreamId, {"value":humidity.toFixed(2)}, function(response) {
-    	    console.log(response.json);
-	});
+    m2x.devices.setStreamValue(deviceId, tempStreamId, { value: temperature.toFixed(2) }, function(response) {
+      console.log(response.json);
     });
+
+    m2x.devices.setStreamValue(deviceId, humidStreamId, { value: humidity.toFixed(2) }, function(response) {
+      console.log(response.json);
+    });
+  });
 });
