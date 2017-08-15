d83297b2a0d9b47fb4068eb589c74849dd3f361f
ihysk <sirouma.09@gmail.com>
2017-08-15 15:33:35 +0000

    Added a functionality to post data to twitter

--- bme280.js
+++ bme280.js
@@ -1,7 +1,14 @@
 const BME280 = require('node-bme280');
 const M2X = require('m2x');
+const twit = require('twit');
 
 const barometer = new BME280({ address: 0x76 });
+const t = new twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+})
 
 const key = process.env.M2X_KEY;
 const deviceId = process.env.M2X_DEVICEID;
@@ -18,10 +25,12 @@ barometer.begin(function(err) {
   console.info('barometer running');
 
   barometer.readPressureAndTemparature(function(err, pressure, temperature, humidity) {
-    console.info(
-      'temp:', temperature.toFixed(2),
-      '℃  pressur', (pressure / 100).toFixed(2),
-      'hPa  hum:', humidity.toFixed(2), '%');
+    const str = `temp:${temperature.toFixed(2)}℃  pressur${(pressure / 100).toFixed(2)}hPa  hum:${humidity.toFixed(2)}%`
+    console.info(str);
+
+    t.post('statuses/update', { status: str }, function(err, data) {
+      console.log(data)
+    })
 
     m2x.devices.setStreamValue(deviceId, tempStreamId, { value: temperature.toFixed(2) }, function(response) {
       console.log(response.json);
